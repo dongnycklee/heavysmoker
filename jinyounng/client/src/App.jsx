aa79f110320c1d61f7de3accb0e092bbef093058
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2021-11-05 17:57:39 +0900

    client  core menu
    client core menu
    
    const menu style css

--- jinyounng/client/src/App.jsx
+++ jinyounng/client/src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import MainRouter from './MainRouter';
-import ScrollToTop from './ScrollToTop';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import MainRouter from "./MainRouter";
+import ScrollToTop from "./ScrollToTop";
 
 const App = () => (
   <BrowserRouter>
@@ -10,6 +10,5 @@ const App = () => (
     </ScrollToTop>
   </BrowserRouter>
 );
-
 
 export default App;
