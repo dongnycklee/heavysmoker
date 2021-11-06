21791e7a9e126bfccf9873732fd0954fe6ac6937
mnnm <alsrhld101@naver.com>
2021-11-06 17:30:03 +0900

    SocialLogin Edit
    구글 인증 클라이언트키 내 걸로 바꾸고 로그인 되면 메인으로 연결되게 해줌

--- hyunho/client/src/auth/PrivateRoute.jsx
+++ hyunho/client/src/auth/PrivateRoute.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { isAuthenticated } from './index';
 
-const PrivateRoute = ({ component: Component, ...rest}) => (
-    <Route {...rest} render={ props => isAuthenticated() ? (
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated() ? (
         <Component {...props} />
-    ) : (
-            <Redirect 
-                to={{
-                    pathname: "/signin",
-                    state: {from: props.location}
-                }}
-            />
-        )}
-    />
-)
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/',
+            state: { from: props.location },
+          }}
+        />
+      )
+    }
+  />
+);
 
 export default PrivateRoute;
