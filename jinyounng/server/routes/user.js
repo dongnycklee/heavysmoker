7b39b82a97b404cf3e99062a836fae6dd5c29c5c
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2021-11-08 18:08:51 +0900

    main pull news and mp4

--- jinyounng/server/routes/user.js
+++ jinyounng/server/routes/user.js
@@ -1,38 +1,25 @@
-const express = require("express");
+const express = require('express')
 
-const {
-    userById,
-    allUsers,
-    getUser,
-    updateUser,
-    deleteUser,
-    userPhoto,
-    addFollowing,
-    addFollower,
-    removeFollowing,
-    removeFollower,
-    findPeople,
-    updateUserRn,
-    userlike,
-} = require("../controllers/user");
-const { requireSignin } = require("../controllers/auth");
+const { userById, allUsers, getUser, updateUser, deleteUser, userPhoto, addFollowing, addFollower, removeFollowing, removeFollower, findPeople, updateUserRn } = require('../controllers/user');
+const { requireSignin } = require('../controllers/auth');
+
 
 const router = express.Router();
 
-router.put("/user/follow", requireSignin, addFollowing, addFollower);
-router.put("/user/unfollow", requireSignin, removeFollowing, removeFollower);
-router.put("/user/:userId", requireSignin, updateUser);
-router.put("/rn/user/:userId", requireSignin, updateUserRn);
+router.put('/user/follow', requireSignin, addFollowing, addFollower);    
+router.put('/user/unfollow', requireSignin, removeFollowing, removeFollower );    
 
 router.get("/users", allUsers);
 router.get("/user/:userId", requireSignin, getUser);
+router.put("/user/:userId", requireSignin, updateUser);
+router.put("/rn/user/:userId", requireSignin, updateUserRn);
+router.delete("/user/:userId", requireSignin, deleteUser);
+
 //photo
 router.get("/user/photo/:userId", userPhoto);
 
 // follow suggestions
 router.get("/user/findpeople/:userId", requireSignin, findPeople);
-
-router.delete("/user/:userId", requireSignin, deleteUser);
 
 // any route containing :userId, this is execute first
 router.param("userId", userById);
