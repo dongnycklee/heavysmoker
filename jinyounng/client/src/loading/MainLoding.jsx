09e61240df5dd7738776e3b9ec14087d0d783395
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2021-11-08 09:42:20 +0900

    test MainLoding

---
+++ jinyounng/client/src/loading/MainLoding.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./mainloding.css";
+
+const MainLoding = () => (
+    <div className="mainloder">
+        <h1>GangsterTalk</h1>
+        <span></span>
+    </div>
+);
+
+export default MainLoding;
