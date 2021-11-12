ad274895b635d8721a8045a7c33df8192c5385da
coolmarvel <marvel97@naver.com>
2021-11-12 12:49:28 +0900

    toast 수정

--- sns/smoker_talk/client/src/components/alert/Toast.jsx
+++ sns/smoker_talk/client/src/components/alert/Toast.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Toast = ({msg, handleShow, bgColor}) => {
+const Toast = ({ msg, handleShow, bgColor }) => {
+    const [show, setShow] = useState(false);
     return (
         <div className={`toast show position-fixed text-light ${bgColor}`}
-        style={{top: '5px', right: '5px', minWidth: '200px', zIndex: 50}}>
+            style={{ top: '5px', right: '5px', minWidth: '200px', zIndex: 50 }}
+            onClose={() => setShow(false)} show={show} delay={3000} autohide>
             <div className={`toast-header text-light ${bgColor}`}>
                 <strong className="mr-auto text-light">{msg.title}</strong>
                 <button className="ml-2 mb-1 close text-light"
-                data-dismiss="toast" style={{outline: 'none'}}
-                onClick={handleShow}>
+                    data-dismiss="toast" style={{ outline: 'none' }}
+                    onClick={handleShow}>
                     &times;
                 </button>
             </div>
@@ -17,6 +19,7 @@ const Toast = ({msg, handleShow, bgColor}) => {
             </div>
         </div>
     )
+
 }
 
 export default Toast
