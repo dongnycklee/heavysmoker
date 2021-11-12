b50eba395f19b23e907c744a9b4a53e7d4b6e976
coolmarvel <marvel97@naver.com>
2021-11-12 14:06:26 +0900

    toast edit

--- sns/smoker_talk/client/src/components/alert/Toast.jsx
+++ sns/smoker_talk/client/src/components/alert/Toast.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react'
 
 const Toast = ({ msg, handleShow, bgColor }) => {
-    const [show, setShow] = useState(false);
     return (
         <div className={`toast show position-fixed text-light ${bgColor}`}
-            style={{ top: '5px', right: '5px', minWidth: '200px', zIndex: 50 }}
-            onClose={() => setShow(false)} show={show} delay={3000} autohide>
+            style={{ top: '50%', right: '20%', minWidth: '200px', zIndex: 50 }}>
             <div className={`toast-header text-light ${bgColor}`}>
                 <strong className="mr-auto text-light">{msg.title}</strong>
                 <button className="ml-2 mb-1 close text-light"
